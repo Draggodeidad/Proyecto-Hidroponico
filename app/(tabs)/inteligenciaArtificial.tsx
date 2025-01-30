41ac237e714a296418f038e250f745282ee3c880
Draggodeidad <antonioimanol7@gmail.com>
2025-01-29 23:56:34 -0600

    feat: Barra de Navegacion Implementada

---
+++ app/(tabs)/inteligenciaArtificial.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function inteligenciaScreen() {
+  return (
+    <View>
+      <Text>IA Screen</Text>
+    </View>
+  );
+}
---
+++ app/(tabs)/monitoreo.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function MonitoreoScreen() {
+  return (
+    <View>
+      <Text>Monitoreo Screen</Text>
+    </View>
+  );
+}
